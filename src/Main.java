433b4772511d8e1b9bf4b42413ada6fbb0e4ce84
adelcioalmeida <adelciopaty@gmail.com>
2025-12-13 20:14:21 -0300

    Adiciona if else para verificar idade

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
     public static void main(String[] args){
-     System.out.println("Olá, mundo!");
+
+        int idade =  18;
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade");
+        } else {
+            System.out.println("Você é menor de idade");
+        }
 
     }
 }
