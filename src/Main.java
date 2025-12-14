01d3ebc7760debcc5d7e915dafaa9b1a0358e4fd
adelcioalmeida <adelciopaty@gmail.com>
2025-12-14 16:46:48 -0300

    Usa array e for para listar idades

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
-        int opcao = -1;
+        int[] idades = {18, 25, 30, 15, 40};
 
-        while (opcao != 0) {
-            mostrarMenu();
-            opcao = entrada.nextInt();
-
-            if (opcao == 1) {
-                dizerOla();
-            } else if (opcao == 2) {
-                mostrarIdade();
-            } else if (opcao == 0) {
-                System.out.println("Saindo do programa...");
-            } else {
-                System.out.println("Opção invalida.");
-            }
-            System.out.println();
+        for (int idade : idades) {
+            System.out.println("Idade: " + idade);
         }
-
-        entrada.close();
-    }
-
-static void mostrarMenu() {
-    System.out.println("MENU");
-    System.out.println("1 - Dizer Olá");
-    System.out.println("2 - Mostrar Idade");
-    System.out.println("0 - Sair");
-    System.out.print("Escolha uma opção:");
-}
-
-static void dizerOla() {
-    System.out.println("Olá, Adelcio!");
-}
-static void mostrarIdade() {
-    System.out.println("Você tem 35 anos.");
 
     }
 }
